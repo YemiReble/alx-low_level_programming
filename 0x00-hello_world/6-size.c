d73eae2b5591d1687d63134a245010920695c89a
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-14 03:14:18 -0700

    Print Data Type Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	long long longlongintegerType;
 	float floatType;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of int: %ld byte(s)\n", sizeof(integerType));
-	printf("Size of long int: %ld byte(s)\n", sizeof(longintegerType));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(longlongintegerType));
-	printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintegerType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintegerType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
