50172851f3a241b0503b6fc8a8cda3d89a225fd7
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-17 05:24:39 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	int m = 0;
+
+	while (m < 10)
+	{
+		printf("%d", m);
+		m = m + 1;
+	}
+
+	return (0);
+
+}
