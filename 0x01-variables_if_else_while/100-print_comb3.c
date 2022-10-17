6ddd4ba5ebf3e9ed70e5b2a3e1fe172b0c922530
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-17 15:20:39 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,33 +8,6 @@
 *
 */
 
-/*int main(void)
-{
-	int l, e;
-	int de1 = 0;
-	int de2 = 0;
-
-	l = 48;
-	while (l < 58)
-	{
-	e = l + 1;
-	while (e < 58)
-	{
-		putchar (de1);
-		putchar (de2);
-		putchar (l);
-		putchar (e);
-		e++;
-		de1 = ',';
-		de2 = ' ';
-	}
-		l++;
-	}
-	putchar (10);
-
-	return (0);
-}*/
-
 int main(void)
 {
 	int d1, d2, n = 0;
