730718ffac56128b4a8a532e551c23529d9a09e4
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-17 11:40:05 -0700

    persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		putchar (m);
+		putchar (',');
+		putchar (' ');
+	}
+	putchar ('\n');
+
+	return (0);
+}
