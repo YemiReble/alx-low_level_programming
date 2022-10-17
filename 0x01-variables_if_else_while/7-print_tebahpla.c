578544c52b029e853abe2dcd06d4f90fd0a12f96
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-17 08:00:14 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	int m;
+
+	for(m ='z';m<='a'; m--)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}
