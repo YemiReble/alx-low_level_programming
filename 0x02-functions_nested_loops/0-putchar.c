875c3370be90d251229350d160ba98665a8bc597
Abodunrin Sulaimon <abodunrinsulimon1@gmail.com>
2022-10-18 10:49:34 -0700

    _Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+
+	//Write a program that prints _putchar, followed by a new line
+
+	printf("_putchar\n");
+
+	return (0);
+
+}
+
+
