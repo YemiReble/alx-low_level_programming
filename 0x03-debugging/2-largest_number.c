c9a97d6adc2a6ed6b6942ff2863f450d8e2bcd06
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-10-21 14:39:20 +0000

    Largest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a)
 	{
 		largest = b;
 	}
