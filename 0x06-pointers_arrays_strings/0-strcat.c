d43726cd8c949a35786320802eb337457fd0a896
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-10-28 21:00:41 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strcat - Concatinating two Strings
+  * @dest: Destination pointer
+  * @src: Source pointer
+  * Return: dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int m, n;
+
+	m = 0;
+	n = 0;
+
+	while (*(dest + m) != '\0')
+	{
+	m++;
+	}
+
+	while (n >= 0)
+	{
+		*(dest + m) += *(src + n);
+
+		if (*(src + n) == '\0')
+			break;
+
+		m++;
+		n++;
+	}
+
+	return (dest);
+}
