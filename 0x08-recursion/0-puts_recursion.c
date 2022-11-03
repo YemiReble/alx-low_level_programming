84fe2f331b91cc8a2f51ef2da463dc110563a9f9
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-03 15:41:45 +0000

    Print a String

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,15 @@
   *
   * Return: Strings *s
   */
+void _puts_recursion(char *s);
 
 void _puts_recursion(char *s)
 {
-	_putchar(s);
+	if(*s != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar("\n");
 }
