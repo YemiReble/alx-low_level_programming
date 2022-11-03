84aa8d19acd1f48d8ed5b54236fc63c6f55c739e
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-03 23:15:46 +0000

    Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,18 @@
 
 int factorial(int n)
 {
-	if (n != '\0')
-		return (n * factorial(n + 1));
-	return (0);
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 1)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
