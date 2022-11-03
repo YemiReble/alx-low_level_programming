5eab7a08612ea7ce57c8f5183371905230bda246
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-03 14:58:23 +0000

    Print a String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - Function that prints strings
+  * @s: Pointer to a string
+  *
+  * Return: Strings *s
+  */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
