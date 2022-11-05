853d699c43ea25fb0b0c4e7ba74bc04cd16547bb
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-05 15:57:48 +0000

    Number of Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - This is a program that prints its name, followed by a new line
+  * @argc: being the variable that prints the number of argument pass
+  * @argv: being the pointer array that prints the characters received by
+  * this programe.
+  * Return: arguments.
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
