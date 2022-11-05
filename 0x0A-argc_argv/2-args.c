bfc77b1ea731078d9cfb4cfdf615e3aed4dd5080
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-05 16:59:19 +0000

    Argument Passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - This is a program that prints its name, followed by a new line
+  * @argc: being the variable that prints the number of argument pass
+  * @argv: being the pointer array that prints the characters received by
+  * this programe.
+  * Return: arguments.
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
