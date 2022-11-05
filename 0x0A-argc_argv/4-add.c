4ced05c93b3ea6169d8a859baeb44cb8c0d2d002
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-05 21:32:28 +0000

    Add Args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - This is a program that prints its name, followed by a new line
+  * @argc: being the variable that prints the number of argument pass
+  * @argv: being the pointer array that prints the characters received by
+  * this programe.
+  * Return: arguments.
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int add;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
