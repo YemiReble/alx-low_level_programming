fbb213488877e474dd63bbb57790c20269457b36
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-05 17:56:22 +0000

    Multiply Arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,20 @@
 int main(int argc, char *argv[])
 {
 	int i;
+
 	int result = 1;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			/*printf("%s\n", argv[i]);*/
 			int mul = strtol(argv[i], NULL, 10);
+
 			result = result * mul;
 		}
 		printf("%d\n", result);
 	}
-	else 
+	else
 	{
 		printf("Error\n");
 	}
