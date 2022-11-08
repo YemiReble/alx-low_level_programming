2720660347d8436186fdfd6cd21ab7159adb565c
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-08 14:00:06 +0000

    Adding Arguments&Error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+  * Number - Cheking the condition of argument valuse
+  * @str: Pointer to holds numubers
+  * Return: nothing
+  */
+
+int isNumber(char *str)
+{
+	int j = strlen(str);
+	
+	while (j--)
+	{
+		if (str[j] > 47 && str[j] < 58)
+			continue;
+		return (0);
+	}
+	
+	return (1);
+}
+
 
 /**
   * main - This is a program that prints its name, followed by a new line
@@ -11,21 +34,28 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int add;
+	int i, sum = 0, count = 0;
 
-	if (argc > 1)
+	if (argc - 1 > 0)
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 1; i < argc; ++i)
 		{
-			add += atoi(argv[i]);
+			if (!isNumber(argv[i]))
+				++count;
+			else
+				sum += atoi(argv[i]);
 		}
-		printf("%d\n", add);
 	}
-	else
+	if (argc - 1 == 0 || count == argc - 1)
+		printf("0\n");
+
+	else if (count > 0)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+		printf("%d\n", sum);
+
 	return (0);
 }
