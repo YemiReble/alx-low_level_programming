0d3bfce82216ebc942e62f766bdacc9b1e29a280
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-25 14:42:40 +0000

    Linked Lists Header File

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,4 +29,8 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+/*for advanced task*/
+void print_str(void)__attribute__((constructor));
+
+
 #endif /*LIST_H*/
