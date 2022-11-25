6dbb3cc7351625917bc17b3a31d8f0d8ce321b03
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-25 14:38:22 +0000

    Function That Execute Before Main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * print_str - check the code
+  *
+  * Return: strings
+  */
+
+void print_str(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
