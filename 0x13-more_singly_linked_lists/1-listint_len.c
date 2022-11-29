2e156046f315de9c48726ebfe69541788c3cdbed
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-11-29 16:44:39 +0000

    Number of element in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t i = 0;
+	const listint_t *tmp = h;
+	size_t count = 0;
 
-	while (h)
+	while (tmp != NULL)
 	{
-		i++;
-		h = h->next;
+		count++;
+		tmp = tmp->next;
 	}
-	return (i);
+	return (count);
 }
