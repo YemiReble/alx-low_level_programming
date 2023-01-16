a2aebf66d1312706f4c90b4268e1914a7875998a
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-16 16:48:12 +0000

    100 Operations Dynamic Lib Header File

---
+++ 0x18-dynamic_libraries/opps.h
@@ -0,0 +1,12 @@
+#ifndef OPPS_H
+#define OPPS_H
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+#endif /* OPPS_H */
